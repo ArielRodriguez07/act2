7f5ac627eebcdc6dcae7bed08b3f331e3cd6c60b
ARIEL_RODRIGUEZ_CENICEROS <a.22308051280706@cbtis128.edu.mx>
2025-03-20 18:27:06 +0000

    act22

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,56 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Alumnos'),
+          backgroundColor: Colors.purple,
+        ),
+        body: Center(
+          child: Container(
+            width: 300,
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.purple[100],
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Card(
+                  child: ListTile(
+                    title: Text('Nombre del alumno: Juan Pérez'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    title: Text('Grado de escuela: 5to grado'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    title: Text('Edad: 10 años'),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
